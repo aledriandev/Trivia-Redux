05ff162fabd6d946b61ec0553ad44aa612f17347
AQP20171679 <alejandraadriantejada@gmail.com>
2017-10-24 06:17:51 -0500

    muestra respuestas

--- src/App.js
+++ src/App.js
@@ -5,36 +5,36 @@ import fb from "./img/fb.png";
 import tw from "./img/tw.png";
 import gl from "./img/g+.png";
 import { connect } from "redux-zero/react";
-import {nextQz} from './actions';
-import {} from 'react-bootstrap'
+import { nextQz } from './actions';
+import { Row, Col } from 'react-bootstrap'
 
 const Choices = ({ choices, next }) => {
   const letters = ['A', 'B', 'C'];
   return choices.map((choice, index) => {
     return (
-      <div key={'a' + index} className='col-lg-4 col-md-4 col-sm-6 col-xs-12'>
-        <button className='btn-question' onClick={()=>next(choice)}>
+      <Col key={'a' + index} lg={4} md={4} sm={6} xs={12}>
+        <button className='btn-question' onClick={() => next(choice)}>
           <span className='letter'>{letters[index]}</span>
           <p>{choice}</p>
           <div className='div-check'></div>
           <div className='div-option'></div>
         </button>
-      </div>);
+      </Col>);
   });
 }
 const Questions = ({ question, choices, next }) => {
   return (
     <div className="bg-white text-center game">
       <h3>{question}</h3>
-      <div className="row text-center choices">
+      <Row className="text-center choices">
         <Choices choices={choices} next={next} />
-      </div>
+      </Row>
       <Social />
     </div>
   );
 }
 
-const Social = ({}) => {
+const Social = ({ }) => {
   return (
     <div className="social">
       <div className='circle'><img src={fb} alt="" /></div>
@@ -44,7 +44,25 @@ const Social = ({}) => {
   );
 }
 
-const App = ({ questions, counter }) => {
+
+const Answers = ({ answers, questions }) => {
+  answers = answers.map((answer, index) => {
+    return (
+      <div key={index}>
+        <p><b>{questions[index].question}: </b>{answer}</p>
+      </div>
+    );
+  });
+  return (
+    <div>
+      <h3>Here are you answers:</h3>
+      {answers}
+      <button className='btn-quiz' onClick={e => this.showSolution(e)}>Submit</button>
+    </div>
+  );
+}
+
+const App = ({ questions, counter, answers }) => {
   return (
     <section className="container">
       <div className="text-center abc-game">
@@ -52,18 +70,28 @@ const App = ({ questions, counter }) => {
         <div className='text-left'><img className='btn-direction' src="img/left.svg" onClick={() => { this.prev() }} alt="" /></div>
         <div><img src={questions[counter].img} /></div>
         <p className='text-left'>{counter} of 5 answered</p>
-        <Questions 
-          question={questions[counter].question} 
-          choices={questions[counter].choices} 
-          next={nextQz}
-        />
-        <div className="bg-white text-center result"></div>
+        {
+          counter < 5
+          &&
+          <Questions
+            question={questions[counter].question}
+            choices={questions[counter].choices}
+            next={nextQz}
+          />
+        }
+        {
+          counter == 5
+          &&
+          <div className="bg-white text-center result">
+            <Answers answers={answers} questions={questions}/>
+          </div>
+        }
         <div className="bg-white text-center again"></div>
       </div>
     </section>
   );
 }
 
-const mapToProps = ({ questions, counter }) => ({ questions, counter });
+const mapToProps = ({ questions, counter, answers }) => ({ questions, counter, answers });
 
 export default connect(mapToProps)(App);
